2f292a1cfa00f975cbcbaebedaf00649afe61096
Ellyf Bernardo <ellyfbernardo12@gmail.com>
2024-11-23 20:28:20 -0300

    trying to make work the scroll when user click on the vote

--- src/app/components/moviecard/moviecard.js
+++ src/app/components/moviecard/moviecard.js
@@ -42,7 +42,18 @@ const MovieCard = (props) => {
     const nextIndex = currentIndex + 1;
 
     if (nextIndex < topicos.length) {
-      cardRefs[topicos[nextIndex]].current.scrollIntoView({ behavior: 'smooth' });
+      const nextCard = cardRefs[topicos[nextIndex]].current;
+      
+      if (nextCard) {
+        const isMobile = window.innerWidth <= 768;
+
+        if (isMobile) {
+          const offset = nextCard.offsetTop - 100; // Ajuste para o topo
+          window.scrollTo({ top: offset, behavior: 'smooth' });
+        } else {
+          nextCard.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
     }
   };
 
@@ -110,8 +121,6 @@ const MovieCard = (props) => {
         </div>
       </div>
 
-      {/* Repita o mesmo padrão para o terceiro filme, adicionando a referência */}
-      {/* Exemplo: <div className={style.container_flex} ref={cardRefs.filme3}> */}
 
       <footer className={style.footer}>
         {hasVotes && (
