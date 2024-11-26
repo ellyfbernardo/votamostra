515b8984de4f2311e792215fbd67e248c40b0a4b
Ellyf Bernardo <ellyfbernardo12@gmail.com>
2024-11-25 23:38:48 -0300

    launching

--- src/app/page.js
+++ src/app/page.js
@@ -33,12 +33,12 @@ export default function Home() {
             
             {/* /mostra-panorama-25-11 */}
           <Link href="/mostra-panorama-25-11" className={styles.link}>
-            {/* <button className={styles.li}>INICIAR VOTAÇÃO</button>  */}
-            <div className={styles.waiting}>
+            <button className={styles.li}>INICIAR VOTAÇÃO</button> 
+            {/* <div className={styles.waiting}>
               <h2 className={styles.aguarde}>AGUARDE!</h2>
               <h3 className={styles.votacao}>A VOTAÇÃO COMEÇARÁ</h3>
               <h3 className={styles.termino}>AO TÉRMINO DA SESSÃO</h3>
-            </div>
+            </div> */}
           </Link>
 
 
